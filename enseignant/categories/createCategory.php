ae596c6db5d71aeab67da9f8266811ee9485ef54
IUB_LOKI <ayoubouharda33@gmail.com>
2025-12-17 17:02:00 +0100

    la creation du categorie

---
+++ enseignant/categories/createCategory.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once __DIR__ . '/../../config/database.php';
+
+/* Sécurité */
+if (!isset($_SESSION['id_utilisateur']) || $_SESSION['role'] !== 'enseignant') {
+    header("Location: ../../auth/login.php");
+    exit;
+}
+
+/* Vérifier soumission */
+if (isset($_POST['nom_categorie'])) {
+
+    $nom = trim($_POST['nom_categorie']);
+    $description = trim($_POST['description']);
+
+    if ($nom === '') {
+        header("Location: categories.php?error=nom");
+        exit;
+    }
+
+    $sql = "INSERT INTO categories (nom_categorie, description, createdAtCat)
+            VALUES (?, ?, NOW())";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$nom, $description]);
+
+    header("Location: categories.php?success=1");
+    exit;
+}
